3ff63ba98b2f97f93a9f407b23970e551d93c8d8
maristizabalo95 <maristizabalo95@gmail.com>
2022-05-24 22:36:25 -0500

    onceavo cambio 24052022

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -145,7 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 STATICFILES_DIRS = [os.path.join('ecommerce/static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
