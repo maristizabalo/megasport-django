593b63ae5d5f119af223f9e28c6bd108721f97df
maristizabalo95 <maristizabalo95@gmail.com>
2022-05-24 21:13:51 -0500

    cuarto cambio 24052022

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b7@1_tci2&k8izv1@ybywoo(u_zef6%ucvow2y0x=#dwmj((&d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['137.184.71.89', 'localhost']
 
