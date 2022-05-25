7528c6030babb120b28c459cfbd1fb9a47cac4bc
maristizabalo95 <maristizabalo95@gmail.com>
2022-05-24 21:23:02 -0500

    quinto cambio 24052022

--- cuentas/views.py
+++ cuentas/views.py
@@ -12,7 +12,6 @@ from django.utils.encoding import force_bytes
 from django.core.mail import EmailMessage
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
-import requests
 
 # Create your views here.
 
