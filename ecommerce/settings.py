be9a4b8b86ddbad5da78c41433aab4b4df8b9691
maristizabalo95 <maristizabalo95@gmail.com>
2022-05-25 22:05:46 -0500

    treceavo 25052022

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -140,18 +140,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     ('ecommerce/static')
 ]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
-    messages.ERROR : 'danger',
+    messages.ERROR: 'danger',
 }
 
 EMAIL_HOST = 'smtp.gmail.com'
